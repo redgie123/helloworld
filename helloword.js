3892696970b40cc3826cf37e18e92c6287d83185
Don Carpenter <donc@xmarc.com>
2017-02-06 18:24:39 +1000

    another message change

--- helloword.js
+++ helloword.js
@@ -2,6 +2,6 @@ function greet(greeting, name) {
     console.log(`${greeting} ${name}`);
 }
 
-greet('Have a great night of continual gaming', 'Zoe');
+greet('Have a great night of study', 'Zoe');
 
 console.log('Finished');
